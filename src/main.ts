2b5f85cb85044129c684e7d40a3efbece04118c9
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-24 17:33:59 +0100

    Inicio

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+const reservas = [
+  {
+    id: 23453,
+    precio: 250,
+    habitacion: "standard",
+    prepago: false,
+    completadaConExito: true,
+  },
+  {
+    id: 56456,
+    precio: 150,
+    habitacion: "superior",
+    prepago: false,
+    completadaConExito: true,
+  },
+  {
+    id: 43243,
+    precio: 550,
+    habitacion: "standard",
+    prepago: true,
+    completadaConExito: false,
+  },
+  {
+    id: 23223,
+    precio: 550,
+    habitacion: "standard",
+    prepago: true,
+    completadaConExito: true,
+  },
+  {
+    id: 89232,
+    precio: 650,
+    habitacion: "superior",
+    prepago: true,
+    completadaConExito: false,
+  },
+];
